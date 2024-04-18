bd20befd9bfe946be56049428f0930444022e1da
swapna reniguntla <swapnar.gitacc@gmail.com>
2024-04-18 08:21:22 +0530

    first commit

---
+++ src/main/java/com/crimsonlogic/spring/jpa/onetoone/repository/TutorialDetailsRepository.java
@@ -0,0 +1,17 @@
+package com.crimsonlogic.spring.jpa.onetoone.repository;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crimsonlogic.spring.jpa.onetoone.model.TutorialDetails;
+
+@Repository
+public interface TutorialDetailsRepository extends JpaRepository<TutorialDetails, Long> {
+  @Transactional
+  void deleteById(long id);
+  
+  @Transactional
+  void deleteByTutorialId(long tutorialId);
+}
